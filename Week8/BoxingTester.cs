e012eea780feb951e1ba9568a7e266887aec9b50
Софiя <sofiia.horenko.25@kse.org.ua>
2026-03-28 18:42:26 +0200

    added pointRef.cs and BoxingTester.s

---
+++ Week8/BoxingTester.cs
@@ -0,0 +1,14 @@
+﻿namespace Week8;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10; // value type stack
+        object b = a; // boxing - value copy to heap
+        int c = int(b); // unboxing - heap to stack
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"b = {b}");
+        Console.WriteLine($"c = {c}");
+    }
+}
