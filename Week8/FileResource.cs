fdcdd6930043c89441304a672756ca3886f46c32
Софiя <sofiia.horenko.25@kse.org.ua>
2026-03-28 17:23:44 +0200

    Added Resources.cs; FileResources.cs; NetworkResources.cs

---
+++ Week8/FileResource.cs
@@ -0,0 +1,27 @@
+﻿namespace Week8;
+
+public class FileResource : Resource, IDisposable
+{
+    public FileResource(string name): base(name) {}
+
+    public override void Open()
+    {
+        IsOpen = true;
+        Console.WriteLine($"File {Name} is open");
+    }
+
+    public override void Close()
+    {
+        if (IsOpen)
+        {
+            IsOpen = false;
+            Console.WriteLine($"File {Name} is close");
+        }
+    }
+
+    public void Dispose()
+    {
+        Close();
+        Console.WriteLine($"File {Name} is dispose");
+    }
+}
---
+++ Week8/NetworkResource.cs
@@ -0,0 +1,27 @@
+﻿namespace Week8;
+
+public class NetworkResource : Resource, IDisposable
+{
+   public NetworkResource(string name) : base(name) {}
+
+   public override void Open()
+   {
+      IsOpen = true;
+      Console.WriteLine($"Network {Name} connected");
+   }
+
+   public override void Close()
+   {
+      if (IsOpen)
+      {
+         IsOpen = false;
+         Console.WriteLine($"Network {Name} Disconnected");
+      }
+   }
+
+   public void Dispose()
+   {
+      Close();
+      Console.WriteLine($"Network {Name} is dispose");
+   }
+}
