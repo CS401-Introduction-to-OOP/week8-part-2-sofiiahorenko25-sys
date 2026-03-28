22277c568f83ac2d1e11308f779c9753233e94f9
Софiя <sofiia.horenko.25@kse.org.ua>
2026-03-28 17:31:36 +0200

    adedd ResourceManager.cs

---
+++ Week8/ResourceManager.cs
@@ -0,0 +1,27 @@
+﻿namespace Week8;
+
+public class ResourceManager<T> where T: Resource
+{
+    private readonly List<T> _resources = new();
+
+    public void Add(T resource)
+    {
+        _resources.Add(resource);
+    }
+
+    public void OpenAll()
+    {
+        foreach (var resource in _resources)
+        {
+            resource.Open();
+        }
+    }
+
+    public void CloseAll()
+    {
+        foreach (var resource in _resources)
+        {
+            resource.Close();
+        }
+    }
+}
