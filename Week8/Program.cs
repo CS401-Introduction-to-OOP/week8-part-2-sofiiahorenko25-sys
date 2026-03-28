c9e8725d95fe83888b27de4321805602a791dd51
Софiя <sofiia.horenko.25@kse.org.ua>
2026-03-28 17:38:51 +0200

    changed program.cs

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,14 @@
-﻿
-class Program
+﻿using Week8;
+var file = new FileResource("report.txt");
+var network = new NetworkResource("api.company.local");
+var manager = new ResourceManager<Resource>();
+manager.Add(file);
+manager.Add(network);
+manager.OpenAll();
+
+using (var tempFile = new FileResource("temp.txt"))
 {
-    public static void Main()
-    {
-        
-    }
-    
+    tempFile.Open();
 }
+manager.CloseAll();
+Console.WriteLine("Done");
