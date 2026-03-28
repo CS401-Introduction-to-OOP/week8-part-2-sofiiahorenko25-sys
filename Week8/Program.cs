1aae1c5d6a8a5bb98ef328020b2d1f7bce2fd8d6
Софiя <sofiia.horenko.25@kse.org.ua>
2026-03-28 19:21:30 +0200

    changed Program.cs

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,17 @@
-﻿
-class Program
+﻿using Week8;
+
+var container = new DataContainer<DataItem>();
+for (int i = 0; i < 10000; i++)
 {
-    public static void Main()
+    var item = new DataItem()
     {
-        
-    }
-    
+        Id = $"item-{i}",
+        Payload = new byte[1024]
+    };
+    container.Add(item);
 }
+Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+GC.Collect();
+GC.WaitForPendingFinalizers();
+Console.WriteLine("GC.Collect() was called.");
+Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
