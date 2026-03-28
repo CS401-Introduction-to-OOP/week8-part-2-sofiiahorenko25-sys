0581e37969191e5351db343477afc7c30c791c07
Софiя <sofiia.horenko.25@kse.org.ua>
2026-03-28 19:13:23 +0200

    Added DataContainer.cs

---
+++ Week8/DataContainer.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+namespace Week8;
+
+public class DataContainer<T> where T: DataItem
+{
+    private readonly List<T> _items = new();
+
+    public void Add(T item)
+    {
+        _items.Add(item);
+    }
+
+    public long GetTotalSize()
+    {
+        long total = 0;
+        foreach (var item in _items)
+        {
+            total += item.GetSize();
+        }
+
+        return total;
+    }
+}
